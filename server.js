7985f2fcec8c8a44cf4d3a2d1870ab6f7496f458
sebastienlucotte <sebastien@rubiomonocoat.fr>
2026-03-26 16:41:59 +0100

    server.js

--- server.js
+++ server.js
@@ -46,43 +46,110 @@ const CONTACTS = {
 };
 
 const ROUTING = {
-  "02": CONTACTS.baptiste, "08": CONTACTS.baptiste, "27": CONTACTS.baptiste,
-  "51": CONTACTS.baptiste, "59": CONTACTS.baptiste, "60": CONTACTS.baptiste,
-  "62": CONTACTS.baptiste, "75": CONTACTS.baptiste, "76": CONTACTS.baptiste,
-  "77": CONTACTS.baptiste, "78": CONTACTS.baptiste, "80": CONTACTS.baptiste,
-  "91": CONTACTS.baptiste, "92": CONTACTS.baptiste, "93": CONTACTS.baptiste,
-  "94": CONTACTS.baptiste, "95": CONTACTS.baptiste,
+  // Nord / IDF
+  "02": CONTACTS.baptiste,
+  "08": CONTACTS.baptiste,
+  "27": CONTACTS.baptiste,
+  "51": CONTACTS.baptiste,
+  "59": CONTACTS.baptiste,
+  "60": CONTACTS.baptiste,
+  "62": CONTACTS.baptiste,
+  "75": CONTACTS.baptiste,
+  "76": CONTACTS.baptiste,
+  "77": CONTACTS.baptiste,
+  "78": CONTACTS.baptiste,
+  "80": CONTACTS.baptiste,
+  "91": CONTACTS.baptiste,
+  "92": CONTACTS.baptiste,
+  "93": CONTACTS.baptiste,
+  "94": CONTACTS.baptiste,
+  "95": CONTACTS.baptiste,
 
-  "03": CONTACTS.guillaume, "14": CONTACTS.guillaume, "18": CONTACTS.guillaume,
-  "22": CONTACTS.guillaume, "28": CONTACTS.guillaume, "29": CONTACTS.guillaume,
-  "35": CONTACTS.guillaume, "36": CONTACTS.guillaume, "37": CONTACTS.guillaume,
-  "41": CONTACTS.guillaume, "44": CONTACTS.guillaume, "45": CONTACTS.guillaume,
-  "49": CONTACTS.guillaume, "50": CONTACTS.guillaume, "53": CONTACTS.guillaume,
-  "56": CONTACTS.guillaume, "58": CONTACTS.guillaume, "61": CONTACTS.guillaume,
-  "72": CONTACTS.guillaume, "85": CONTACTS.guillaume, "89": CONTACTS.guillaume,
+  // Ouest
+  "03": CONTACTS.guillaume,
+  "14": CONTACTS.guillaume,
+  "18": CONTACTS.guillaume,
+  "22": CONTACTS.guillaume,
+  "28": CONTACTS.guillaume,
+  "29": CONTACTS.guillaume,
+  "35": CONTACTS.guillaume,
+  "36": CONTACTS.guillaume,
+  "37": CONTACTS.guillaume,
+  "41": CONTACTS.guillaume,
+  "44": CONTACTS.guillaume,
+  "45": CONTACTS.guillaume,
+  "49": CONTACTS.guillaume,
+  "50": CONTACTS.guillaume,
+  "53": CONTACTS.guillaume,
+  "56": CONTACTS.guillaume,
+  "58": CONTACTS.guillaume,
+  "61": CONTACTS.guillaume,
+  "72": CONTACTS.guillaume,
+  "85": CONTACTS.guillaume,
+  "89": CONTACTS.guillaume,
 
-  "09": CONTACTS.laurent, "16": CONTACTS.laurent, "17": CONTACTS.laurent,
-  "19": CONTACTS.laurent, "23": CONTACTS.laurent, "24": CONTACTS.laurent,
-  "31": CONTACTS.laurent, "32": CONTACTS.laurent, "33": CONTACTS.laurent,
-  "40": CONTACTS.laurent, "46": CONTACTS.laurent, "47": CONTACTS.laurent,
-  "64": CONTACTS.laurent, "65": CONTACTS.laurent, "79": CONTACTS.laurent,
-  "81": CONTACTS.laurent, "82": CONTACTS.laurent, "86": CONTACTS.laurent,
+  // Sud-Ouest
+  "09": CONTACTS.laurent,
+  "16": CONTACTS.laurent,
+  "17": CONTACTS.laurent,
+  "19": CONTACTS.laurent,
+  "23": CONTACTS.laurent,
+  "24": CONTACTS.laurent,
+  "31": CONTACTS.laurent,
+  "32": CONTACTS.laurent,
+  "33": CONTACTS.laurent,
+  "40": CONTACTS.laurent,
+  "46": CONTACTS.laurent,
+  "47": CONTACTS.laurent,
+  "64": CONTACTS.laurent,
+  "65": CONTACTS.laurent,
+  "79": CONTACTS.laurent,
+  "81": CONTACTS.laurent,
+  "82": CONTACTS.laurent,
+  "86": CONTACTS.laurent,
   "87": CONTACTS.laurent,
 
-  "01": CONTACTS.antony, "10": CONTACTS.antony, "21": CONTACTS.antony,
-  "25": CONTACTS.antony, "39": CONTACTS.antony, "52": CONTACTS.antony,
-  "54": CONTACTS.antony, "55": CONTACTS.antony, "57": CONTACTS.antony,
-  "67": CONTACTS.antony, "68": CONTACTS.antony, "70": CONTACTS.antony,
-  "71": CONTACTS.antony, "88": CONTACTS.antony, "90": CONTACTS.antony,
+  // Est
+  "01": CONTACTS.antony,
+  "10": CONTACTS.antony,
+  "21": CONTACTS.antony,
+  "25": CONTACTS.antony,
+  "39": CONTACTS.antony,
+  "52": CONTACTS.antony,
+  "54": CONTACTS.antony,
+  "55": CONTACTS.antony,
+  "57": CONTACTS.antony,
+  "67": CONTACTS.antony,
+  "68": CONTACTS.antony,
+  "70": CONTACTS.antony,
+  "71": CONTACTS.antony,
+  "88": CONTACTS.antony,
+  "90": CONTACTS.antony,
 
-  "04": CONTACTS.benjamin, "05": CONTACTS.benjamin, "06": CONTACTS.benjamin,
-  "07": CONTACTS.benjamin, "11": CONTACTS.benjamin, "12": CONTACTS.benjamin,
-  "13": CONTACTS.benjamin, "15": CONTACTS.benjamin, "20": CONTACTS.benjamin,
-  "26": CONTACTS.benjamin, "30": CONTACTS.benjamin, "34": CONTACTS.benjamin,
-  "38": CONTACTS.benjamin, "42": CONTACTS.benjamin, "43": CONTACTS.benjamin,
-  "48": CONTACTS.benjamin, "63": CONTACTS.benjamin, "66": CONTACTS.benjamin,
-  "69": CONTACTS.benjamin, "73": CONTACTS.benjamin, "74": CONTACTS.benjamin,
-  "83": CONTACTS.benjamin, "84": CONTACTS.benjamin,
+  // Sud-Est
+  "04": CONTACTS.benjamin,
+  "05": CONTACTS.benjamin,
+  "06": CONTACTS.benjamin,
+  "07": CONTACTS.benjamin,
+  "11": CONTACTS.benjamin,
+  "12": CONTACTS.benjamin,
+  "13": CONTACTS.benjamin,
+  "15": CONTACTS.benjamin,
+  "20": CONTACTS.benjamin, // Corse = 2A + 2B
+  "26": CONTACTS.benjamin,
+  "30": CONTACTS.benjamin,
+  "34": CONTACTS.benjamin,
+  "38": CONTACTS.benjamin,
+  "42": CONTACTS.benjamin,
+  "43": CONTACTS.benjamin,
+  "48": CONTACTS.benjamin,
+  "63": CONTACTS.benjamin,
+  "66": CONTACTS.benjamin,
+  "69": CONTACTS.benjamin,
+  "73": CONTACTS.benjamin,
+  "74": CONTACTS.benjamin,
+  "83": CONTACTS.benjamin,
+  "84": CONTACTS.benjamin,
 };
 
 function checkAuth(req, res, next) {
@@ -102,16 +169,21 @@ function normalizeCode(input) {
 
   let code = String(input).trim().toUpperCase();
 
-  // retire un éventuel #
+  // retire le #
   code = code.replace(/#/g, "");
 
-  if (code === "2A" || code === "2B" || code === "20") return "20";
+  // gestion Corse
+  if (code === "2A" || code === "2B" || code === "20") {
+    return "20";
+  }
 
   // garde uniquement les chiffres
   code = code.replace(/\D/g, "");
 
   // complète si un seul chiffre
-  if (code.length === 1) code = "0" + code;
+  if (code.length === 1) {
+    code = "0" + code;
+  }
 
   return code.length === 2 ? code : null;
 }
@@ -188,15 +260,15 @@ app.post("/aircall/smart-routing", checkAuth, (req, res) => {
   res.json({
     routing: {
       targetType: result.contact.targetType,
-      targetValue: result.contact.targetValue
+      targetValue: result.contact.targetValue,
     },
     meta: {
-      rawCode,
-      rawAttempts,
+      receivedDepartmentCode: rawCode,
+      receivedAttempts: rawAttempts,
       normalizedCode: result.code,
       reason: result.reason,
-      targetName: result.contact.name
-    }
+      selected: result.contact.name,
+    },
   });
 });
 
